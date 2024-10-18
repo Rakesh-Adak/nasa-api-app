5f433b44387b7a4ede25f819e782095f4ffda7ca
Rakesh Adak <rakeshadak099@gmail.com>
2024-10-18 15:15:47 +0530

    First commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,7 +2,7 @@ export default function Main(props) {
     const {data} = props
     return(
         <div className="imageContainer">
-             <img src={data?.hdurl} className="bgImage" alt={data?.title || background-image}/>
+             <img src={data?.hdurl} className="bgImage" alt={data?.title}/>
         </div>
     )
 }
