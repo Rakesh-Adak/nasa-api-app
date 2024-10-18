405e347d5d484c30dce52b2ad3b2a04631fa0b66
Rakesh Adak <rakeshadak099@gmail.com>
2024-10-18 15:44:31 +0530

    Updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer(props) {
         <h2>{data?.title}</h2>
       </div>
       <button onClick={handleToggleModal}>
-        <i className="fa-solid fa-circle-info"></i>
+      <i className="fa-solid fa-circle-arrow-right"></i>
       </button>
     </footer>
   );
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,14 +4,16 @@ export default function Sidebar(props) {
     <div className="sidebar">
       <div className="bgOverlay"></div>
       <div className="sidebarContents">
-        <h2>{data?.title}</h2>
+        <h2>
+          {data?.title}
+          <button onClick={handleToggleModal}>
+          <i className="fa-solid fa-x"></i>
+          </button>
+        </h2>
         <div className="descriptionContainer">
           <p className="descriptionTitle">{data?.date}</p>
           <p>{data?.explanation}</p>
         </div>
-        <button onClick={handleToggleModal}>
-          <i className="fa-solid fa-arrow-right"></i>
-        </button>
       </div>
     </div>
   );
